14787544b6a95e3406a20d9f814708674002c460
Ben Wolfaardt <benjaminwolfaardt@gmail.com>
2023-10-31 23:03:23 +0100

    Finish "Store Object"

---
+++ x/checkers/testutil/constants.go
@@ -0,0 +1,7 @@
+package testutil
+
+const (
+	Alice = "cosmos1ct8cukjn5yvs97js4s3693glq20xqfj39s2wa8"
+	Bob   = "cosmos1gl5r2askkdw4cm5gga66qj0vsa0j32vlklz5nf"
+	Carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
+)
